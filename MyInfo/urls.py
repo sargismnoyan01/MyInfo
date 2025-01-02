8fb2501743e154d38f5d0066bd51c3a3dbbd8668
sargismnoyan01 <128826963+sargismnoyan01@users.noreply.github.com>
2025-01-03 00:52:45 +0400

    Update urls.py

--- MyInfo/urls.py
+++ MyInfo/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('sargismnoyanadminpanel_727447Sa__/', admin.site.urls),
     path('',include('main.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
